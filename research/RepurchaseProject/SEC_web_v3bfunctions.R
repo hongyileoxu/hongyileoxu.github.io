cc024cb3b84dfaef098b5d74fef8d028f170ded5
hongyileoxu <86061901+hongyileoxu@users.noreply.github.com>
2023-07-04 16:03:07 +0200

    Update SEC_web_v3bfunctions.R
    unexpected string constant.

--- research/RepurchaseProject/SEC_web_v3bfunctions.R
+++ research/RepurchaseProject/SEC_web_v3bfunctions.R
@@ -139,9 +139,7 @@ filing.item <- function(x, # filing
                         item_id, # the identifier from 'href' for the section 
                         item, # the regex for the item (item number./ item name)
                         filing_qrt, # the quarter the filing was made 
-                        text_break_node, # the xml to replace the identified table
-                        table = TRUE, # whether to scrap the table numbers 
-                        parts = c("footnote") # the parts of information that you want 
+                        text_break_node # the xml to replace the identified table
 ) { 
   # extract info from the section/item 
   if (loc_item[1] == loc_item[2]) {
@@ -295,8 +293,8 @@ filing.cleaned <- function(loc_file, # name of the filing
                                  item_id = loc_item2$item_id,
                                  item = loc_item2$item_id,
                                  text_break_node = text_break_node, 
-                                 filing_qrt = str_extract(loc_file, pattern = '(QTR\\d{1})'),
-                                 parts = "footnote")
+                                 filing_qrt = str_extract(loc_file, pattern = '(QTR\\d{1})')
+                                )
   }
   ## return output 
   return(c(list(info = info), # store header info
