6ff34e931418b2078e55ebb907797acf1cd3391d
hongyileoxu <86061901+hongyileoxu@users.noreply.github.com>
2023-07-04 15:28:17 +0200

    Update SEC_web_v3bfunctions.R
    also correct pattern in `item_txt` in function `filing.item`.

--- research/RepurchaseProject/SEC_web_v3bfunctions.R
+++ research/RepurchaseProject/SEC_web_v3bfunctions.R
@@ -146,7 +146,7 @@ filing.item <- function(x, # filing
     if (any(is.na(item_id)) == TRUE) {
       # print("no item_id")
       item_parse <- sub(pattern = paste(".*", item[1], sep = "")[1], "", x[loc_item[1]], ignore.case = T)
-      item_txt <- sub(pattern = "[>][itemITEM]{4}.*", "", item_parse) 
+      item_txt <- sub(pattern = "(>?)[itemITEM]{4}.*", "", item_parse) 
     } else {
       print("Yes! item_id")
       item_parse <- sub(pattern = paste(".*(\"|\')", item_id[1], "(\"|\')", sep = "")[1], "", x[loc_item[1]])
