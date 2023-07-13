0a062da9dd0676994494667f064f564e5cdd140b
hongyileoxu <86061901+hongyileoxu@users.noreply.github.com>
2023-07-13 12:43:56 +0200

    Update SEC_web_v3cfunctions.R
    update `item_tbl_id` to find the correct table.

--- research/RepurchaseProject/SEC_web_v3cfunctions.R
+++ research/RepurchaseProject/SEC_web_v3cfunctions.R
@@ -280,7 +280,9 @@ filing.item <- function(x, # filing
                 parts = html_text(item_html, trim = T),  
                 table_unit = NA))
   } else { # if there are tables!
-    item_tbl_id <- which(str_count(pattern = "/tr", as.character(item_tbls)) > 1 & grepl(pattern = "Total.*Number|purchase|repurchase", x = item_tbls, ignore.case = T))[1] # identify the correct table
+    item_tbl_id <- which(str_count(string = as.character(item_tbls), pattern = "/tr") > 1 & # number of rows > 1
+                           str_count(string = as.character(item_tbls), pattern = "/td") > 6 & # number of columns > 6
+                           grepl(pattern = "Total.*Number|purchase|repurchase", x = item_tbls, ignore.case = T))[1] # identify the correct table
     
     ## extract the table 
     if (ifelse(is.na(item_tbl_id), 
